7ea8bb322670d33f392762b5fb2157732f784d3c
Trần Trọng Trí <161337259+TriItMyName@users.noreply.github.com>
2026-04-07 21:45:20 +0700

    Update root route to serve login.html instead of menu.html

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/src', express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'menu.html'));
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 app.listen(PORT, () => {
